b4c094494ef976e53f862b76742bdb6bb4553137
DiegoCaruanaMontes <131860298+DiegoCaruanaMontes@users.noreply.github.com>
2024-04-30 16:35:05 +0200

    Add files via upload

---
+++ ROS_TELECO/ros_teleco/src/publisher_2.cpp
@@ -0,0 +1,39 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    // Initialize the ROS node
+    ros::init(argc, argv, "publisher_2");
+
+    // Create a ROS node handle
+    ros::NodeHandle nh;
+
+    // Create a publisher that publishes messages of type std_msgs::String on the "your_topic" topic
+    ros::Publisher publisher = nh.advertise<std_msgs::String>("topic_2", 10);
+
+    // Set the loop rate (in Hz)
+    ros::Rate loop_rate(1);
+
+    while (ros::ok())
+    {
+        // Create a string message
+        std_msgs::String msg;
+        std::cin>>msg.data;
+
+        // Publish the message
+        publisher.publish(msg);
+
+        // Log the message to the console
+        ROS_INFO("Publishing: %s", msg.data.c_str());
+
+        // Spin once to let callbacks run
+        ros::spinOnce();
+
+        // Sleep to achieve the desired loop rate
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
---
+++ ROS_TELECO/ros_teleco/src/subscriber_2.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void callback(const std_msgs::String::ConstPtr& msg)
+{
+    // Callback function that is executed whenever a message is received
+
+    // Print the received message to the console
+    ROS_INFO("Received: %s", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+    // Initialize the ROS node
+    ros::init(argc, argv, "subscriber_2");
+
+    // Create a ROS node handle
+    ros::NodeHandle nh;
+
+    // Create a subscriber that subscribes to messages of type std_msgs::String on the "your_topic" topic
+    ros::Subscriber subscriber = nh.subscribe("topic_2", 10, callback);
+
+    // Spin to let callbacks run
+    ros::spin();
+
+    return 0;
+}
